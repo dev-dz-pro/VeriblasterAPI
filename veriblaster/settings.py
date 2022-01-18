3117208bb9365910a5f6c8c95e396a9e0136a1e2
dev dz <beiniptv.tools@gmail.com>
2022-01-19 00:15:46 +0100

    verib api update 12:15

--- veriblaster/settings.py
+++ veriblaster/settings.py
@@ -56,6 +56,9 @@ MIDDLEWARE = [
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+    'DEFAULT_RENDERER_CLASSES': (
+        'rest_framework.renderers.JSONRenderer',
     )
 }
 
